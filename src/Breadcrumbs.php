5439d5e53128b64fb15c46a42cd76fbd14d7fd9b
lamalamaMark <mark@lamalama.nl>
2020-06-10 11:02:47 +0200

    wip

--- src/Breadcrumbs.php
+++ src/Breadcrumbs.php
@@ -6,11 +6,20 @@ use Illuminate\Support\Facades\View;
 
 class Breadcrumbs
 {
+    /**
+     * trail.
+     * @param  array $parts
+     * @return void
+     */
     public static function trail($parts)
     {
+        if (! $parts) {
+            return View::share('breadcrumbs', false);
+        }
+
         $divider = '<svg class="w-3 h-3 mx-1" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 5l7 7-7 7"></path></svg>';
 
-        $html = '<ol itemscope itemtype="https://schema.org/BreadcrumbList">';
+        $html = '<ol class="flex items-center text-gray-500" itemscope itemtype="https://schema.org/BreadcrumbList">';
 
         $i = 1;
         foreach ($parts as $label => $path) {
@@ -20,10 +29,27 @@ class Breadcrumbs
                             <span itemprop="name">'.$label.'</span></a>
                         <meta itemprop="position" content="'.$i.'" />
                       </li>';
+
+            if ($i < count($parts)) {
+                $html .= '<svg class="w-3 h-3 mx-3" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 5l7 7-7 7"></path></svg>';
+            }
+
             $i++;
         }
         $html .= '</ol>';
 
-        View::share('breadcrumbs', $html);
+        return View::share('breadcrumbs', $html);
+    }
+
+    /**
+     * generate.
+     * @param  boolean $minify
+     * @return void
+     */
+    public static function generate($minify = false)
+    {
+        $html = '';
+
+        return ($minify) ? str_replace(PHP_EOL, '', $html) : $html;
     }
 }
