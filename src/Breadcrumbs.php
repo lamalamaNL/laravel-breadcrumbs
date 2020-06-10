c343672faa0509775ffdbe5d6f3b051fadbd42e6
lamalamaMark <mark@lamalama.nl>
2020-06-11 00:21:49 +0200

    wip

--- src/Breadcrumbs.php
+++ src/Breadcrumbs.php
@@ -15,8 +15,12 @@ class Breadcrumbs
      * @param  array $parts
      * @return void
      */
-    public function trail($parts)
+    public function trail($parts = false)
     {
+        if (! $parts) {
+            return;
+        }
+
         $divider = '<svg class="w-3 h-3 mx-1" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 5l7 7-7 7"></path></svg>';
 
         $this->html = '<ol class="flex items-center text-gray-500" itemscope itemtype="https://schema.org/BreadcrumbList">';
